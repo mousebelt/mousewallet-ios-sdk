3ca802abd2f508f3dfe31fe05bb132e1fcb4a3fd
Jonathan Kirknes <j.kirknes68@gmail.com>
2018-05-02 06:02:53 +1000

    Initial Commit

---
+++ NRLWalletSDK/NRLWalletSDK.h
@@ -0,0 +1,19 @@
+//
+//  NRLWalletSDK.h
+//  NRLWalletSDK
+//
+//  Created by Wang Gang on 5/2/18.
+//  Copyright © 2018 NoRestLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NRLWalletSDK.
+FOUNDATION_EXPORT double NRLWalletSDKVersionNumber;
+
+//! Project version string for NRLWalletSDK.
+FOUNDATION_EXPORT const unsigned char NRLWalletSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NRLWalletSDK/PublicHeader.h>
+
+
