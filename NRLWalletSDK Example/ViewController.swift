662b459504b7a8263d2b61f91190977608acbaa9
bdavid68 <b.david6008@gmail.com>
2018-05-21 20:11:17 +1000

    update framework links

--- NRLWalletSDK Example/ViewController.swift
+++ NRLWalletSDK Example/ViewController.swift
@@ -9,6 +9,24 @@
 import UIKit
 import NRLWalletSDK
 
+//data extension to convert binary data to hex string
+extension Data {
+    struct HexEncodingOptions: OptionSet {
+        let rawValue: Int
+        static let upperCase = HexEncodingOptions(rawValue: 1 << 0)
+    }
+    
+    func hexEncodedString(options: HexEncodingOptions = []) -> String {
+        let hexDigits = Array((options.contains(.upperCase) ? "0123456789ABCDEF" : "0123456789abcdef").utf16)
+        var chars: [unichar] = []
+        chars.reserveCapacity(2 * count)
+        for byte in self {
+            chars.append(hexDigits[Int(byte / 16)])
+            chars.append(hexDigits[Int(byte % 16)])
+        }
+        return String(utf16CodeUnits: chars, count: chars.count)
+    }
+}
 
 class ViewController: UIViewController {
 
@@ -21,7 +39,7 @@ class ViewController: UIViewController {
             print("mnemonic = \(mnemonic.joined(separator: " "))")
 
             let seed = try NRLMnemonic.mnemonicToSeed(from: mnemonic, withPassphrase: "Test")
-            print("\nseed = \(seed.toHexString())")
+            print("\nseed = \(seed.hexEncodedString())")
             
             // Ethereum : 60
             let etherWallet = NRLWallet(seed: seed, network: .main(.ethereum))
